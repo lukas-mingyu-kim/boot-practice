6e0d5434981b4a5030c3b303b756fce7d8b07151
Lukas Kim <lukaskim@umich.edu>
2020-04-08 07:16:02 +0900

    post put, get API added

--- src/main/java/com/example/bootpractice/domain/posts/PostsRepository.java
+++ src/main/java/com/example/bootpractice/domain/posts/PostsRepository.java
@@ -3,5 +3,4 @@ package com.example.bootpractice.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
 }
